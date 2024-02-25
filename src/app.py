8ada898251f3cd4af5ffbecb1c3066137f628112
Celia Elias <148253357+celiaelias@users.noreply.github.com>
2024-02-25 22:42:23 +0000

    feat: todo list API

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 @app.route('/todos', methods=['GET'])
-def hello_world():
+def get_todos():
     json_text = jsonify(todos)
     return json_text
 
